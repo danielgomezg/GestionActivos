c306d6e59b181439ce5d6d71f39c2912387bdd1c
danielgomezg <daniel.gomez@usach.cl>
2023-11-14 22:28:36 -0300

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,24 @@
+<script>
+import { Router, Link, Route } from "svelte-routing";
+import Login from "./views/login.svelte";
+import Movies from "./views/movies.svelte";
+import {user} from "./stores/user"
+import Nav from "./components/nav.svelte";
+import Perfil from "./views/perfil.svelte";
+
+</script>
+
+<div>
+  <Router>
+    {#if $user}
+       
+    {/if}
+    <Route path="/"> <Login></Login> </Route>
+    <Route path="/movies"> <Movies></Movies> </Route>
+    <Route path="/perfil"> <Perfil></Perfil> </Route>
+  </Router>
+</div>
+
+<style>
+  
+</style>
---
+++ src/stores/user.js
@@ -0,0 +1,19 @@
+import { writable } from 'svelte/store'
+
+const createUser = () => {
+    const { subscribe, set } = writable(localStorage.user ? JSON.parse(localStorage.getItem("user")): null)
+
+    return {
+        subscribe,
+        setUser: (user) => {
+            set(user)
+        }
+    }
+}
+
+export const user = createUser()
+
+/*
+export const user = writable(
+    localStorage.user ? JSON.parse(localStorage.getItem("user")): null
+)*/
