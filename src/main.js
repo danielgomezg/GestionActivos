08d7c9995064b0be3793cb44003f2e9c28274580
mauricarvdiaz <mauricio.carvajal@usach.cl>
2024-03-16 00:35:59 -0300

    hide console log priduction

--- src/main.js
+++ src/main.js
@@ -3,6 +3,10 @@ import './styles/_material.scss'
 import './styles/_variables.scss'
 import App from './App.svelte'
 
+// if (import.meta.env.MODE == 'production') {
+//   console.log = function() {};
+// }
+
 const app = new App({
   target: document.getElementById('app'),
 })
