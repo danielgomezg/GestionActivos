9e787d6bff91b256f78e2e5a2e9e2afebdf40145
mauricarvdiaz <mauricio.carvajal@usach.cl>
2024-04-23 00:10:56 -0400

    barcodescan 2

--- src/lib/BarcodeScanner.svelte
+++ src/lib/BarcodeScanner.svelte
@@ -12,24 +12,24 @@
                     width: 520,
                     height: 400,                  
                     facingMode: "environment"  //"environment" for back camera, "user" front camera
-                }
+                },
+                area: { // defines rectangle of the detection/localization area
+                    top: "0%",    // top offset
+                    right: "0%",  // right offset
+                    left: "0%",   // left offset
+                    bottom: "0%"  // bottom offset
+                },
             },
             decoder: {
-                readers: ["code_128_reader"],
-                debug: {
-                    showCanvas: true,
-                    showPatches: true,
-                    showFoundPatches: true,
-                    showSkeleton: true,
-                    showLabels: true,
-                    showPatchLabels: true,
-                    showRemainingPatchLabels: true,
-                    boxFromPatches: {
-                        showTransformed: true,
-                        showTransformedBox: true,
-                        showBB: true
-                    }
-                }
+                readers: [ 
+                    "code_128_reader",
+                    "ean_reader",
+                    "ean_8_reader",
+                    "code_39_reader",
+                    "code_39_vin_reader",
+                    "codabar_reader",
+                    "upc_reader",
+                    "upc_e_reader" ] ,
             },
             locator: {
                 patchSize: 'medium',
@@ -46,6 +46,7 @@
         });
 
         Quagga.onDetected((data) => {
+            console.log('Detected', data.codeResult.code);
             console.log(data.codeResult.code);
             Quagga.stop();
         });
