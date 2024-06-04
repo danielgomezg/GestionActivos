bda2c3e33a3f92264d7b09c5541f358867e9557c
mauricarvdiaz <mauricio.carvajal@usach.cl>
2024-06-03 22:03:08 -0400

    create, update values

--- src/components/valores/valueActive.svelte
+++ src/components/valores/valueActive.svelte
@@ -1,34 +1,107 @@
 <script>
     import { TextField, Divider } from "$lib";
+    import Api from "../../../helpers/ApiCall";
+    import { companySelect } from "../../stores/store";
     
     export let active = {};
     export let values = {};
 
+    let copyValues = { ...values }
     let inputVidaUtil;
     let inputValorReal;
     let inputValorAdquisicion;
 
-    const updateValue = (id, amount, value) => {
-        amount = amount.toString().replace(/\./g, '').replace('$', '');
+    const updateValue = async (id, amount, value) => {
+        // amount = amount.toString().replace(/\./g, '').replace('$', '');
 
-        console.log('updateValue')
-        console.log('id', id)
-        console.log('amount', amount)
-        console.log('value', value)
+        console.log('new values => ', values);
+        console.log('old values => ', copyValues);
+
+        let body = {
+            adq_value: values.adq_value.toString().replace(/\./g, '').replace('$', ''),
+            real_value: values.real_value.toString().replace(/\./g, '').replace('$', ''),
+            useful_life: values.useful_life
+        };
+
+        body = JSON.stringify(body);
+
+        console.log('updateValue => ', body);
+        let response = await Api.call(`/active/values/${id}`, 'PUT', { body }, 'json', $companySelect);
+        console.log('RESPONSE PUT ACTIVOS VALUES --> ', response)
+        if (response.success && response.statusCode == "201") {
+            if (value == 'real_value') inputValorReal.setFeefback('success')
+            else if (value == 'adq_value') inputValorAdquisicion.setFeefback('success')
+            else if (value == 'useful_life') inputVidaUtil.setFeefback('success')
+        } 
+        else {
+            if (value == 'real_value') {
+                values.real_value = copyValues.real_value
+                inputValorReal.setFeefback('error')
+            } 
+            else if (value == 'adq_value') {
+                values.adq_value = copyValues.adq_value
+                inputValorAdquisicion.setFeefback('error')
+            } 
+            else if (value == 'useful_life') {
+                values.useful_life = copyValues.useful_life
+                inputVidaUtil.setFeefback('error')
+            } 
+        }
+        
     }
 
-    const createValue = (id, amount, value = '') => {
+    const createValue = async (amount, value = '') => {
         amount = amount.toString().replace(/\./g, '').replace('$', '');
 
-        console.log('createValue')
-        console.log('id', id)
-        console.log('amount', amount)
-        console.log('value', value)
+        console.log('new values => ', values);
+        console.log('old values => ', copyValues);
 
-        if (value == 'valor_real') {
-            inputValorReal.setFeefback('success')
+        console.log(active)
+        let body = {
+            adq_value: 0,
+            real_value: 0,
+            useful_life: 0,
+            active_id: 0
+        };
+        body[value] = amount;
+        body.active_id = active.id;
+        body = JSON.stringify(body);
+
+        console.log('createValue => ', body);
+        let response = await Api.call(`/active/values`, 'POST', { body }, 'json', $companySelect);
+        console.log('RESPONSE POST ACTIVOS VALUES --> ', response)
+        if (response.success && response.statusCode == "201") {
+            values.id = response.data.result.id
+            copyValues = { ...values }
+            if (value == 'real_value') inputValorReal.setFeefback('success')
+            else if (value == 'adq_value') inputValorAdquisicion.setFeefback('success')
+            else if (value == 'useful_life') inputVidaUtil.setFeefback('success')
+        } 
+        else {
+            if (value == 'real_value') {
+                values.real_value = copyValues.real_value
+                inputValorReal.setFeefback('error')
+            } 
+            else if (value == 'adq_value') {
+                values.adq_value = copyValues.adq_value
+                inputValorAdquisicion.setFeefback('error')
+            } 
+            else if (value == 'useful_life') {
+                values.useful_life = copyValues.useful_life
+                inputVidaUtil.setFeefback('error')
+            } 
         }
 
+    }
+
+    const formatLifetime = (lifetime) => {
+        // Quitar caracteres no numericos
+        lifetime = lifetime.toString().replace(/[^0-9]/g, '');
+
+        // Quitar 0 a la izquierda
+        lifetime = lifetime.replace(/^0+/, '');
+
+        return lifetime
     }
 
     const formatNumber = (number) => {
@@ -50,7 +123,7 @@
 
     $: values.real_value = formatNumber(values.real_value)
     $: values.adq_value = formatNumber(values.adq_value)
-    $: values.useful_life = values.useful_life.toString().replace(/[^0-9]/g, '');
+    $: values.useful_life = formatLifetime(values.useful_life)
     
 
 </script>
@@ -62,7 +135,7 @@
         bind:this={inputValorReal}
         bind:value={ values.real_value }  
         on:blur={() => {
-            if (values.id == 0) createValue(values.id, values.real_value, 'real_value') 
+            if (values.id == 0) createValue(values.real_value, 'real_value') 
             else updateValue(values.id, values.real_value, 'real_value')
         }}
 
@@ -73,7 +146,7 @@
         bind:this={inputValorAdquisicion}
         bind:value={ values.adq_value } 
         on:blur={() => {
-            if (values.id == 0) createValue(values.id, values.adq_value, 'adq_value') 
+            if (values.id == 0) createValue(values.adq_value, 'adq_value') 
             else updateValue(values.id, values.adq_value, 'adq_value')
         }}
     />
@@ -83,7 +156,7 @@
         bind:this={inputVidaUtil}
         bind:value={ values.useful_life } 
         on:blur={() => {
-            if (values.id == 0) createValue(values.id, values.useful_life, 'useful_life') 
+            if (values.id == 0) createValue(values.useful_life, 'useful_life') 
             else updateValue(values.id, values.useful_life, 'useful_life')
         }}
     />
